460033da24de9c3db28032e2e3ba20c5bd09c6f6
Krishna <krishs09@yahoo.in>
2023-02-27 20:37:57 +0530

    eureka

---
+++ eureka-module/src/test/java/com/ctgh/EurekaModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctgh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
